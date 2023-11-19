822d8e3deff7704172259366d6e59f08c7028aae
Dan Bitter <bitter.daniel@gmail.com>
2023-11-19 12:42:11 -0500

    1 - 2 44.4

--- 1. Two sum/1.java
+++ 1. Two sum/1.java
@@ -1,18 +1,18 @@
+import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ret = new int[2];
+        HashMap<Integer, Integer> indicies = new HashMap<Integer, Integer>();
         for (int ik=0;ik<nums.length;ik++) {
-            for (int jk=ik+1;jk<nums.length;jk++) {
-                if ((nums[ik] + nums[jk]) == target) {
-                    ret[1] = ik;
-                    ret[0] = jk;
-                    ik = nums.length;
-                    jk = nums.length;
-                    break;
-                }
+            int curr = nums[ik];
+            int other = target-curr;
+
+            int val = (indicies.get(other) != null ? (int)indicies.get(other).intValue() : -1);
+            if(val > -1) {
+                return new int[] { ik, val };
+            } else {
+                indicies.put(new Integer(curr), new Integer(ik));
             }
         }
-        return ret;
+        return new int[] { 0,0 };
     }
-    void p(String m) { System.out.println(m); }
 }
